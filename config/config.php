1196bda5af5d0a5a9b95d8bb1694c49a98334666
Tobias Wolf <tobias.wolf@wagnerwagner.de>
2025-11-13 13:54:05 +0100

    Add default options to config.php
    Signed-off-by: Tobias Wolf <tobias.wolf@wagnerwagner.de>

--- config/config.php
+++ config/config.php
@@ -6,18 +6,30 @@ return [
 	'ordersPage' => 'orders',
 	'production' => false,
 	'api.endpoint' => 'shop',
-	// 'taxRules' => [
-	// 	'default' => [
-	// 		'name' => fn (): string => t('taxRule.default'),
-	// 		'rule' => fn (?App $kirby): float => 19,
-	// 	],
-	// ],
-	// 'pricingRules' => [
-	// 	'default' => [
-	// 		'name' => 'default',
-	// 		'currency' => 'EUR',
-	// 		'rule' => fn (?App $kirby): bool => true,
-	// 		'taxIncluded' => true,
-	// 	],
-	// ],
+	'taxRules' => [
+		'default' => [
+			'name' => fn (): string => t('taxRule.default'),
+			'rule' => fn (?App $kirby): float => 19,
+		],
+	],
+	'pricingRules' => [
+		'default' => [
+			'name' => 'default',
+			'currency' => 'EUR',
+			'rule' => fn (?App $kirby): bool => true,
+			'taxIncluded' => true,
+		],
+	],
+	'stripe.test.publishable_key' => '',
+	'stripe.test.secret_key' => '',
+	'stripe.live.publishable_key' => '',
+	'stripe.live.secret_key' => '',
+	'paypal.sandbox.clientID' => '',
+	'paypal.sandbox.secret' => '',
+	'paypal.live.clientID' => '',
+	'paypal.live.secret' => '',
+	'paypal.applicationContext' => [],
+	'paypal.purchaseUnits' => null, /** fn () => [] */
+	'stripe.webhook_signing_secret' => '',
+	'gateways' => [],
 ];
