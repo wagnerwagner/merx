c26a281aa70f3a80a13a9b43e274434238901ba4
Tobias Wolf <tobias.wolf@wagnerwagner.de>
2025-11-20 14:06:45 +0100

    Move hooks to own directory
    Signed-off-by: Tobias Wolf <tobias.wolf@wagnerwagner.de>

---
+++ config/hooks.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+	'ww.merx.stripe-hooks' => function (\Stripe\Event $stripeEvent): void
+	{
+		switch ($stripeEvent->type) {
+			case 'payment_intent.succeeded':
+				/** @var \Stripe\PaymentIntent $paymentIntent */
+				$paymentIntent = $stripeEvent->data->object;
+				$orderUid = $paymentIntent->metadata->order_uid;
+				if ($orderUid) {
+					try {
+						/** @var ?OrderPage $orderPage */
+						$orderPage = page(option('ww.merx.ordersPage'). '/' . $orderUid);
+						if ($orderPage) {
+							$kirby = $orderPage->kirby();
+							$kirby->impersonate('kirby', function () use ($orderPage, $paymentIntent) {
+								$orderPage?->update([
+									'paymentDetails' => (array)$paymentIntent->toArray(),
+									'paymentComplete' => true,
+									'paidDate' => date('c'),
+								]);
+							});
+						}
+					} catch(Exception) {}
+				}
+				break;
+		}
+	}
+];
--- index.php
+++ index.php
@@ -92,34 +92,7 @@ App::plugin(
 			'de' => include __DIR__ . '/translations/de.php',
 			'en' => include __DIR__ . '/translations/en.php',
 		],
-		'hooks' => [
-			'ww.merx.stripe-hooks' => function (\Stripe\Event $stripeEvent): void
-			{
-				switch ($stripeEvent->type) {
-					case 'payment_intent.succeeded':
-						/** @var \Stripe\PaymentIntent $paymentIntent */
-						$paymentIntent = $stripeEvent->data->object;
-						$orderUid = $paymentIntent->metadata->order_uid;
-						if ($orderUid) {
-							try {
-								/** @var ?OrderPage $orderPage */
-								$orderPage = page(option('ww.merx.ordersPage'). '/' . $orderUid);
-								if ($orderPage) {
-									$kirby = $orderPage->kirby();
-									$kirby->impersonate('kirby', function () use ($orderPage, $paymentIntent) {
-										$orderPage?->update([
-											'paymentDetails' => (array)$paymentIntent->toArray(),
-											'paymentComplete' => true,
-											'paidDate' => date('c'),
-										]);
-									});
-								}
-							} catch(Exception) {}
-						}
-						break;
-				}
-			}
-		],
+		'hooks' => include __DIR__ . '/config/hooks.php',
 		'fieldMethods' => [
 			'toFormattedPrice' => function (Field $field, string|null $currency = null): string
 			{
