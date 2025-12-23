4b8170e9854991154803c931a433c85f80ed6ff5
Tobias Wolf <tobias.wolf@wagnerwagner.de>
2025-12-23 10:43:19 +0100

    Add default hooks
    Signed-off-by: Tobias Wolf <tobias.wolf@wagnerwagner.de>

--- config/hooks.php
+++ config/hooks.php
@@ -30,5 +30,19 @@ return [
 				break;
 		}
 	},
+	'ww.merx.cart.add:after' => function (Cart $cart):void {},
+	'ww.merx.cart.add:before' => function (Cart $cart, array $data):void {},
 	'ww.merx.cart.add:before' => function (Cart $cart, string|array|ListItem $data): void {},
+	'ww.merx.cart.create:after' => function (Cart $cart):void {},
+	'ww.merx.cart.create:before' => function (Cart $cart, array $data):void {},
+	'ww.merx.cart.delete:after' => function (Cart $cart):void {},
+	'ww.merx.cart.delete:before' => function (Cart $cart):void {},
+	'ww.merx.cart.remove:after' => function (Cart $cart, string $key):void {},
+	'ww.merx.cart.remove:before' => function (Cart $cart, string $key):void {},
+	'ww.merx.cart.updateItem:after' => function (Cart $cart, string $key, array $data):void {},
+	'ww.merx.cart.updateItem:before' => function (Cart $cart, string $key, array $data):void {},
+	'ww.merx.initializeOrder:before' => function (Cart $cart, array $data):void {},
+	'ww.merx.initializeOrder:after' => function (OrderPage $virtualOrderPage, string $redirect):void {},
+	'ww.merx.createOrder:before' => function (OrderPage $virtualOrderPage, array $gateway, array $data):void {},
+	'ww.merx.createOrder:after' => function (OrderPage $virtualOrderPage, array $gateway, array $data):void {},
 ];
