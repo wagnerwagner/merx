2f53389cc95341e3005c79e29cc23237d45e9a8b
Tobias Wolf <tobias.wolf@wagnerwagner.de>
2026-01-21 16:08:12 +0100

    Add cache control header to success.php
    Some servers falsely store 302 redirects (resource moved temporarily) in its internal cache (for a short time). This could result in users being redirected to a false order page.
    
    Signed-off-by: Tobias Wolf <tobias.wolf@wagnerwagner.de>

--- templates/success.php
+++ templates/success.php
@@ -1,6 +1,9 @@
 <?php
 
 try {
+    /** SECURITY/PRIVACY: Never (server) cache this response, otherwise user could be redirected to false order page */
+    header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0, s-maxage=0');
+    header('Expires: 0');
     $orderPage = merx()->completePayment($_GET);
     go($orderPage->url());
 } catch (Exception $ex) {
