e31abc07a83151bf47602c3445faf58f1a84e9ce
Tobias Wolf <tobias.wolf@wagnerwagner.de>
2026-02-12 19:28:29 +0100

    Update composer
    Signed-off-by: Tobias Wolf <tobias.wolf@wagnerwagner.de>

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -1,8 +1,8 @@
 <?php return array(
     'root' => array(
         'name' => 'wagnerwagner/merx',
-        'pretty_version' => '2.0.0-alpha.3',
-        'version' => '2.0.0.0-alpha3',
+        'pretty_version' => '2.0.0-alpha.4',
+        'version' => '2.0.0.0-alpha4',
         'reference' => null,
         'type' => 'kirby-plugin',
         'install_path' => __DIR__ . '/../../',
@@ -29,8 +29,8 @@
             'dev_requirement' => false,
         ),
         'wagnerwagner/merx' => array(
-            'pretty_version' => '2.0.0-alpha.3',
-            'version' => '2.0.0.0-alpha3',
+            'pretty_version' => '2.0.0-alpha.4',
+            'version' => '2.0.0.0-alpha4',
             'reference' => null,
             'type' => 'kirby-plugin',
             'install_path' => __DIR__ . '/../../',
