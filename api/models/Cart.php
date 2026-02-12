8681cfc55be6c87ce543376144a474d17a48736f
Tobias Wolf <tobias.wolf@wagnerwagner.de>
2026-02-12 19:27:38 +0100

    Update Cart api model
    Signed-off-by: Tobias Wolf <tobias.wolf@wagnerwagner.de>

--- api/models/Cart.php
+++ api/models/Cart.php
@@ -6,7 +6,7 @@ use Wagnerwagner\Merx\Cart;
 $productList = include 'ProductList.php';
 return [
 	'fields' => array_merge($productList['fields'], [
-		'checkout' => fn (): ?Page => site()->checkoutPage(),
+		'checkout' => fn (): ?Page => $this->site()->checkoutPage(),
 	]),
 	'type' => Cart::class,
 	'views' => array_merge_recursive($productList['views'], [
